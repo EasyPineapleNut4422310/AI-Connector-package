97a0e38c7a24f4d1351f05174e753945267a1540
EasyPineapleNut4422310 <Elijah3650@outlook.com>
2026-04-23 08:06:38 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.20.0")
+        .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.0.0")
     ],
     targets: [
         .executableTarget(
